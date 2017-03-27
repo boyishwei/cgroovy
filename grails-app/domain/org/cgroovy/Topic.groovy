c4474dda72f3a9731532ec97b3bd814f13998f92
Ryan <boyishwei@gmai.com>
2017-03-28 00:03:53 +0800

    Added datasource, and the first data model to project

---
+++ grails-app/domain/org/cgroovy/Topic.groovy
@@ -0,0 +1,11 @@
+package org.cgroovy
+
+class Topic {
+    String userId
+    String title
+    String content
+    Date createTime
+    Date updateTime
+    String lastReplier
+    Integer readTimes
+}
